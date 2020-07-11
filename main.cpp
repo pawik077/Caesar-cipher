48c5ad89c921130c7c0d9e1770969aa26028255a
pawik077 <pawik.r.s.077@gmail.com>
2020-07-11 15:53:25 +0200

    Added main menu stub

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
 int main() {
-
+	int op;
+	std::cout << "Caesar cipher solver\n";
+	std::cout << "Available options:\n";
+	std::cout << "1 - Encrypt input using given key\n";
+	std::cout << "2 - Decrypt input using given key\n";
+	std::cout << "3 - Check if input encrypted correctly\n";
+	std::cout << "Your choice: ";
+	std::cin >> op;
+	while(std::cin.fail() || op > 3 || op < 1) {
+		std::cerr << "Incorrect option!" << std::endl;
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+		std::cout << "Your choice: ";
+		std::cin >> op;
+	}
+	switch(op) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+	}
 }
