88d5a813c3d04a6987db1613c7969db6d9ed7211
pawik077 <pawik.r.s.077@gmail.com>
2020-07-11 20:11:49 +0200

    Fixed space interpreting in decryption, expanded sppecial character handling in encryption

--- Caesar.cpp
+++ Caesar.cpp
@@ -4,7 +4,10 @@ std::string encrypt(const std::string& input, int key) {
 	int k = key % 26;
 	std::string output;
 	for(unsigned int i = 0; i < input.size(); ++i) {
-		if(input[i] >= 0x20 && input[i] <= 0x40 || input[i] >= 0x5b && input[i] <= 0x60) output.push_back(input[i]);
+		if(input[i] >= 0x20 && input[i] <= 0x40 ||
+			 input[i] >= 0x5b && input[i] <= 0x60 ||
+			 input[i] >= 0x7b && input[i] <= 0x7e)
+			output.push_back(input[i]);
 		else if(input[i] >= 0x41 && input[i] <= 0x5a) {
 			if(input[i] + k > 0x5a) output.push_back(input[i] + k - 26);
 			else output.push_back(input[i] + k);
@@ -20,7 +23,7 @@ std::string decrypt(const std::string& input, int key) {
 	int k = key % 26;
 	std::string output;
 	for(int i = 0; i < input.size(); ++i) {
-		if(input[i] >= 0x21 && input[i] <= 0x40 ||
+		if(input[i] >= 0x20 && input[i] <= 0x40 ||
 			 input[i] >= 0x5b && input[i] <= 0x60 ||
 			 input[i] >= 0x7b && input[i] <= 0x7e)
 			output.push_back(input[i]);
